f1fa84d8c0dbb9c9f8e78926dd5cc22a2635d0fd
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-11-07 23:45:15 +0530

    backend setup done

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const db = require("./config/database");
+require("dotenv").config();
+
+port = process.env.PORT;
+
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const uRouter = require("./routers/user.router");
+const pRouter = require("./routers/post.router");
+const ouRouter = require("./routers/otherUser.router");
+  
+app.use(cors());
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", uRouter);
+app.use("/post", pRouter);
+app.use("/oUser", ouRouter);
+
+app.listen(port, () => {
+  db();
+  console.log("server stated on http://localhost:" + port);
+});
